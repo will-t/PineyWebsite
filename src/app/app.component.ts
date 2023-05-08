0c41d5183058efc35195568e91f47e03641e4598
will-t <w98434545@gmail.com>
2023-05-08 10:16:50 -0400

    Added basic framework for two items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public menuSetting: boolean = false;
+  public liveFlag: boolean = false;
+  public PrRequest: boolean = false;
   title = 'PineyBaptistChurch';
+
+
+
+
+
+
+
+
+flipMenu() {
+this.menuSetting = !this.menuSetting;
+return this.menuSetting;
+}
+openLive() {
+this.liveFlag = !this.liveFlag;
+this.menuSetting = false;
+this.PrRequest = false;
+return this.liveFlag;
+}
+openPrRequest() {
+this.PrRequest = !this.PrRequest;
+this.menuSetting = false;
+this.liveFlag = false;
+return this.PrRequest;
+}
 }
